6fcc1a72e2f3548644b2b6c772d0aa06a9b16486
manik1210 <manigane12@gmail.com>
2024-01-04 22:30:02 +0530

    log4j added

--- src/test/java/com/bankingAutomation/testcases/TC_001_LoginTest.java
+++ src/test/java/com/bankingAutomation/testcases/TC_001_LoginTest.java
@@ -8,17 +8,23 @@ public class TC_001_LoginTest extends BaseClass{
     @Test
     public void loginTest(){
         driver.get(baseUrl);
+        logger.info("URL is Opened");
         LoginPage lp= new LoginPage(driver);
         lp.setUsername(userName);
+        logger.info("User name is entered");
         lp.setPassword(passWord);
+        logger.info("Password is entered");
         lp.clickSubmit();
+        logger.info("login button is clicked");
         System.out.println(driver.getTitle());
 
         if(driver.getTitle().equals("Guru99 Bank Manager HomePage")){
             Assert.assertTrue(true);
+            logger.info("Test Passed");
         }
         else{
             Assert.assertFalse(false);
+            logger.info("Test Failed");
         }
 
     }
